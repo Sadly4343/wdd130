7fb9a2d9029c1ffd4b7649c89a648a822ea4f7ca
sadly4343 <loganhartshorn@gmail.com>
2024-04-02 21:33:57 -0600

    commit

---
+++ wwr/styles/Word Game.py
@@ -0,0 +1,18 @@
+word = "apple"
+
+print('Guess a word?')
+guess = input('What is your guess?')
+
+for letter in word:
+    if letter.lower() == guess.lower():
+        print(letter.upper(), end="")
+    else:
+        print(letter.lower(), end="")
+print()
+
+for letter in word:
+    if letter.lower() == guess.lower():
+        print("_", end="")
+    else:
+        print(letter.lower(), end="")
+print()
